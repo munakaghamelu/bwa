4e669cb0c46b9e7e23746ab0d8e20ef4f3a22352
Muna Aghamelu <munagham1@WKMZTC074443.global.publicisgroupe.net>
2022-12-13 09:20:41 +0000

    building out landing

--- bwa/src/components/landing/Landing.jsx
+++ bwa/src/components/landing/Landing.jsx
@@ -3,7 +3,17 @@ import "./landing.scss";
 export default function Landing(){
   return (
     <div className="landing" id="landing">
-      Landing Page
+      <div id="content">
+        <div>
+          <img src='assets/logo.png'/>
+        </div>
+        <div>
+          <h3>Promoting, celebrating, connecting and supporting Black Women Artists.</h3>
+        </div>
+        <div>
+          <img src='assets/background.png'/>
+        </div>
+      </div>
     </div>
   );
 }
