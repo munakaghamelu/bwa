c002aacabf5b944440f0a518ad15626c862abab4
Muna Aghamelu <munagham1@WKMZTC074443.global.publicisgroupe.net>
2022-12-20 16:59:14 +0000

    finished join page + support'

--- bwa/src/components/join/Join.jsx
+++ bwa/src/components/join/Join.jsx
@@ -3,7 +3,24 @@ import "./join.scss";
 export default function Join(){
   return (
     <div className="join" id="join">
-      Join Page
+      <div id="content">
+        <div className="one">
+          <p>
+            Subscribe to our newsletter for updates, opportunity round-ups 
+            and monthly exhibition picks!
+          </p>
+          <a href="#">
+            <img src="assets/subscribe.png"></img>
+          </a>
+          <p>
+            If you are a black woman artist and want to exhibit your work with us, 
+            subscribe to our newsletter and/or follow us on Instagram for Open Calls.
+          </p>
+        </div>
+        <div className="two">
+          <img src="assets/join.png"></img>
+        </div>
+      </div>
     </div>
   );
 }
--- bwa/src/components/support/Support.jsx
+++ bwa/src/components/support/Support.jsx
@@ -3,7 +3,21 @@ import "./support.scss";
 export default function Support(){
   return (
     <div className="support" id="support">
-      Support Page
+      <div id="content">
+        <div className="one">
+          <img src="assets/support.png"></img>
+        </div>
+        <div className="two">
+          <h1>PAYPAL DONATION LINK</h1>
+          <a href="">
+            <img src="assets/paypal.png"></img>
+          </a>
+          <p>
+            As a black-led organisation serving black women, a donation is the 
+            best way to help us support the artist’s we work with.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
