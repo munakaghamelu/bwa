9c5cf1af42ee733fc5ea34e4e7767b0629fc4899
Muna Aghamelu <munagham1@WKMZTC074443.global.publicisgroupe.net>
2022-12-20 16:38:22 +0000

    completed comittee page

--- bwa/src/components/committee/Committee.jsx
+++ bwa/src/components/committee/Committee.jsx
@@ -3,7 +3,36 @@ import "./committee.scss"
 export default function Committee(){
   return (
     <div className="committee" id="committee">
-      Committee Page
+      <div id="content">
+        <div className="one">
+          <img src="assets/muna.png"></img>
+        </div>
+        <div className="two">
+          <h1>MUNA</h1>
+        </div>
+        
+        <div className="three">
+          <img src="assets/sabina.png"></img>
+        </div>
+        <div className="four">
+          <h1>SABINA</h1>
+        </div>
+
+        <div className="five">
+          <img src="assets/louise.png"></img>
+        </div>
+        <div className="six">
+            <h1>LOUISE</h1>
+        </div>
+
+        <div className="seven">
+          <img src="assets/khadija.png"></img>
+        </div>
+        <div className="eight">
+            <h1>KHADIJA</h1>
+        </div>
+
+      </div>
     </div>
   );
 }
