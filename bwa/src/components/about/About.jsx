8b111c3aa698821f0d9da59d900cf56d13274a6f
Muna Aghamelu <munagham1@WKMZTC074443.global.publicisgroupe.net>
2022-12-20 14:29:29 +0000

    fixes stretching of grid, due to missing 'repeat'

--- bwa/src/components/about/About.jsx
+++ bwa/src/components/about/About.jsx
@@ -6,11 +6,11 @@ export default function About() {
       <div id="content">
         <div className="one">1</div>
         <div className="two">
-          {/* <p> Black Woman Artist Network is a non-profit organisation aiming to promote, 
+          <p> Black Woman Artist Network is a non-profit organisation aiming to promote, 
             celebrate, connect and support Black Women/Woman identifying Artists. 
             Through events, exhibitions, lectures and more we aim to champion black 
             female artists and create spaces that celebrate and uplifts them.
-          </p> */}
+          </p>
         </div>
         <div className="three">3</div>
       </div>
