94d8d6ec10ec284f06ad0fe7d45a20e5a724ef50
Muna Aghamelu <munagham1@WKMZTC074443.global.publicisgroupe.net>
2022-12-29 13:27:52 +0000

    added event into events component

--- bwa/src/components/event/Event.jsx
+++ bwa/src/components/event/Event.jsx
@@ -1,17 +1,18 @@
 import "./event.scss"
 
 // pass the data to be loaded to the event from parent (Events) component
-export default function Event({img, title, date, price}) {
+export default function Event({img, title, date, price}) { 
   return (
     <div className="event" id="event">
       <div className="one">
-        <img src={img}/>
+        <h1>img</h1>
+        {/* <img src={img}/> */}
       </div>
       <div className="two">
-        <p>{title} {date} {price}</p>
+        {/* <p>{title} {date} {price}</p> */}
       </div>
       <div className="three">
-        <a href="#"><h1>LEARN MORE</h1></a>
+        {/* <a href="#"><h1>LEARN MORE</h1></a> */}
       </div>
     </div>
   );
--- bwa/src/components/events/Events.jsx
+++ bwa/src/components/events/Events.jsx
@@ -1,9 +1,29 @@
-import "./events.scss"
+import Event from "../event/Event";
+import "./events.scss";
+import "../event/event.scss";
 
 export default function Events(){
   return (
     <div className="events" id="events">
-      Events Page
+      <div id="content">
+
+        <div className="one">
+          <h1><a href="#">PAST</a> | <a href="#">UPCOMING</a></h1>
+        </div>
+
+        <div className="two">
+          <Event/>
+        </div>
+
+        <div className="three">
+          <Event/>
+        </div>
+
+        <div className="four">
+          <h1><a href="#">LOAD MORE</a></h1>
+        </div>
+
+      </div>
     </div>
   );
 }
