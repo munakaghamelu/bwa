25bd15c3c52e9328c279f7687da78bba2f1f5353
Muna Aghamelu <munagham1@WKMZTC074443.global.publicisgroupe.net>
2022-12-29 14:27:49 +0000

    fixed annoying css problem

--- bwa/src/components/event/Event.jsx
+++ bwa/src/components/event/Event.jsx
@@ -4,15 +4,25 @@ import "./event.scss"
 export default function Event({img, title, date, price}) { 
   return (
     <div className="event" id="event">
-      <div className="one">
-        <h1>img</h1>
-        {/* <img src={img}/> */}
-      </div>
-      <div className="two">
-        {/* <p>{title} {date} {price}</p> */}
-      </div>
-      <div className="three">
-        {/* <a href="#"><h1>LEARN MORE</h1></a> */}
+      <div id="content">
+
+        <div className="one">
+          <h1>img</h1>
+          {/* <img src={img}/> */}
+        </div>
+
+        <div className="two">
+          <h1>Title</h1>
+          <h1>Date</h1>
+          <h1>Price</h1>
+          {/* <p>{title} {date} {price}</p> */}
+        </div>
+
+        <div className="three">
+          <h1>LOAD MORE</h1>
+          {/* <a href="#"><h1>LEARN MORE</h1></a> */}
+        </div>
+
       </div>
     </div>
   );
