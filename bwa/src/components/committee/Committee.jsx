3d21e4dc439188bd7392cec71a644df11648e34a
munakaghamelu <mgodwinaghamelu@yahoo.co.uk>
2023-01-30 19:09:23 +0000

    added commit bios

--- bwa/src/components/committee/Committee.jsx
+++ bwa/src/components/committee/Committee.jsx
@@ -7,32 +7,67 @@ export default function Committee(){
         <div className="zero">
           <h1>The Committee</h1>
         </div>
+
         <div className="one">
           <img src="assets/muna.png"></img>
+          <div className="muna_info">
+            <h3>Muna (<a href="https://www.instagram.com/bymuna.ko/?hl=en" target="_blank">@bymuna.ko</a>) 
+            is an Artist and Software Engineer based in London. 
+              You can find her playfully taking over BWA's Instagram 
+              stories, documenting exhibitions she has visited, or coding up the 
+              BWA website. 
+            </h3>
+          </div>
         </div>
+
         <div className="two">
-          <h1>Muna</h1>
+          <h1><a href="https://www.instagram.com/bymuna.ko/?hl=en" target="_blank">Muna</a></h1>
         </div>
         
         <div className="three">
           <img src="assets/sabina.png"></img>
+          <div className="sabina_info">
+            <h3>Contemporary artist and project manager Sabina Silver 
+              (<a href="https://www.instagram.com/sabinasilverx/?hl=en" target="_blank">@sabinasilverx</a>)
+              lives and works in London. She collaborates with the team to advance BWA's purpose and is an art aficionado. 
+              Her primary responsibility inside the organisation is focused largely on highlighting 
+              outstanding and intriguing exhibitions of black female art.
+            </h3>
+          </div>
         </div>
+
         <div className="four">
-          <h1>Sabina</h1>
+          <h1><a href="https://www.instagram.com/sabinasilverx/?hl=en" target="_blank">Sabina</a></h1>
         </div>
 
         <div className="five">
           <img src="assets/louise.png"></img>
+          <div className="louise_info">
+            <h3>Louise (<a href="https://www.instagram.com/louise_hall_art/?hl=en" target="_blank">@louise_hall_art</a>) 
+            is a multidisciplinary artist, workshop facilitator and educator. 
+              You can find Louise running workshops and art lecture series for BWA.
+            </h3>
+          </div>
         </div>
+
         <div className="six">
-            <h1>Louise</h1>
+            <h1><a href="https://www.instagram.com/louise_hall_art/?hl=en" target="_blank">Louise</a></h1>
         </div>
 
         <div className="seven">
           <img src="assets/khadija.png"></img>
+          <div className="khad_info">
+            <h3>Khadija Cecile Niang (<a href="https://www.instagram.com/khadijacecileart/?hl=en" target="_blank">@khadijacecileart</a>)
+              is a Norway-born Senegalese multi-disciplinary artist, 
+              currently based in London. She also works in arts communications and as well 
+              as supporting BWA with events and gallery trips, she curates the newsletter 
+              with opportunities and more.
+            </h3>
+          </div>
         </div>
+
         <div className="eight">
-            <h1>Khadija</h1>
+            <h1><a href="https://www.instagram.com/khadijacecileart/?hl=en" target="_blank">Khadija</a></h1>
         </div>
 
       </div>
--- bwa/src/components/founders/Founders.jsx
+++ bwa/src/components/founders/Founders.jsx
@@ -12,39 +12,31 @@ export default function Founders(){
           <img src="assets/janet.png"></img>
           <div className="janet_info">
             <h3>
-            Janet is a portrait artist and Design Manager based in 
+            Janet (<a href="https://www.instagram.com/_berchie_/?hl=en" target="_blank">@_berchie_</a>) is a portrait artist and Design Manager based in 
             South London. She is one of BWA's co-founders and works 
             with the team to put on a variety of events, exhibitions 
             and find new and exciting ways to promote black female art.
             </h3>
-            <h3>
-              Connect with Janet on Instagram:
-            </h3>
-            <h3><a href="https://www.instagram.com/_berchie_/?hl=en" target="_blank">@_berchie_</a></h3>
           </div>
         </div>
 
         <div className="two">
-          <h1>Janet</h1>
+          <h1><a href="https://www.instagram.com/_berchie_/?hl=en" target="_blank">Janet</a></h1>
         </div>
 
         <div className="three">
           <img src="assets/leah.png"></img>
           <div className="leah_info">
-            <h3>Leah is a portrait artist and Events Manager based in South London. 
+            <h3>Leah (<a href="https://www.instagram.com/leahjeanneart/?hl=en" target="_blank">@leahjeanneart</a>) is a portrait artist and Events Manager based in South London. 
             She is one of BWA's co-founders and works with the team to put on a 
             variety of events, exhibitions and find new and exciting ways to 
             promote black female art.
             </h3>
-            <h3>
-            You can connect with Leah on Instagram:
-            </h3>
-            <h3><a href="https://www.instagram.com/leahjeanneart/?hl=en" target="_blank">@leahjeanneart</a></h3>
           </div>
         </div>
 
         <div className="four">
-          <h1>Leah</h1>
+          <h1><a href="https://www.instagram.com/leahjeanneart/?hl=en" target="_blank">Leah</a></h1>
         </div>
       </div>
     </div>
